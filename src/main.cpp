98544116b17fd0fd1c6e84882e8dc68f4eb9afaa
Daryl Lee <daryl@daryllee.com>
2021-03-20 19:57:24 -0600

    Hello, world, hard-coded

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,21 @@
 #include <tcl/tcl.h>
 
 int main(int argc, char* argv[]) {
+    // Set up Tcl interpreter
+    Tcl_Interp *interp;
+    interp = Tcl_CreateInterp();
+    if (!interp) {
+        std::cerr << "Failed to create Tcl Interpreter" << std::endl;
+        return 1;
+    }
+
+    // Hellow, world from Tcl
+    if (TCL_OK != Tcl_Eval(interp, "puts \"Hello from Tcl\"")) {
+        std::cerr << "Error interpreting script" << std::endl;
+        return 2;
+    }
+
+    // After all else, clean up Tcl
+    Tcl_DeleteInterp(interp);
     return 0;
 }
