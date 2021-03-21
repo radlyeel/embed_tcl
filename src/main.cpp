b7dc1a344a8fc4d113db9480c55ada1efb053919
Daryl Lee <daryl@daryllee.com>
2021-03-21 14:52:50 -0600

    Exercise 1 complete.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,21 @@
   namespace po = boost::program_options;
 #include <tcl/tcl.h>
 
+/* This version supports this level of dialog.  Not that x is
+ * remembered
+
+[daryl@mac-dol-2 build_mb]$./embedtcl
+Tcl: puts "Hello from Tcl"
+Hello from Tcl
+Tcl: puts "again"
+again
+Tcl: set x "Hello X"
+Tcl: puts $x
+Hello X
+Tcl: quit
+[daryl@mac-dol-2 build_mb]$
+
+*/
 int main(int argc, char* argv[]) {
     // Set up Tcl interpreter
     Tcl_Interp *interp;
@@ -13,11 +28,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Hellow, world from Tcl
-    if (TCL_OK != Tcl_Eval(interp, "puts \"Hello from Tcl\"")) {
-        std::cerr << "Error interpreting script" << std::endl;
-        return 2;
+    // Get script from user
+    std::string scr;
+    while (true) {
+        std::cout << "Tcl: ";
+        std::getline(std::cin, scr);
+        if (scr == "quit") {
+            break;
+        }
+        if (TCL_OK != Tcl_Eval(interp, scr.c_str())) {
+            std::cerr << "Error interpreting script" << std::endl;
+            return 2;
+        }
     }
+
 
     // After all else, clean up Tcl
     Tcl_DeleteInterp(interp);
