ee555397b140b73499dac46eee741396dfeb0529
Daryl Lee <daryl@daryllee.com>
2021-03-20 19:42:31 -0600

    Working CMakeLists.txt and near-empty main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+  namespace po = boost::program_options;
+#include <tcl/tcl.h>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
